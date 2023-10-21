26533e71d3bb2556a0aac5ed449ff47096a29c44
LoisClinton <loisclinton@hotmail.co.uk>
2023-10-21 21:38:22 +0100

    Podcast length() method

--- classes/Podcast.js
+++ classes/Podcast.js
@@ -1,12 +1,10 @@
-// host: A string with the name of the host.
-// episodeName: A string with the name of the episode
-// episodeNumber: A number with the episode number.
-// guests: An array of the guests on the particular episode.
-// listen(): Methods that returns the string <TITLE> - Episode: <EPISODE_TITLE>. Hosted by <HOST> and featuring guests <GUESTS>. Length: <LENGTH> seconds.
-
 const Music = require("./Music");
 
 class Podcast extends Music {
+  // host: A string with the name of the host.
+  // episodeName: A string with the name of the episode
+  // episodeNumber: A number with the episode number.
+  // guests: An array of the guests on the particular episode.
   constructor(
     title,
     year,
@@ -24,6 +22,14 @@ class Podcast extends Music {
     this.episodeName = episodeName;
     this.episodeNumber = episodeNumber;
     this.guests = guests;
+  }
+
+  // <TITLE> - Episode: <EPISODE_TITLE>. Hosted by <HOST> and featuring guests <GUESTS>. Length: <LENGTH> seconds.
+
+  listen() {
+    const guestNameAsString = this.guests.join(", ");
+
+    return `${this.title} - Episode: ${this.episodeName}. Hosted by ${this.host} and featuring guests ${guestNameAsString}. Length: ${this.length} seconds.`;
   }
 }
 
